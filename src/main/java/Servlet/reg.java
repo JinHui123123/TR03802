0bf3e9624f1c900324eacdf0b961dcc463c76084
jhui <1831462864@qq.com>
2024-03-27 09:36:15 +0800

    第一次提交

---
+++ src/main/java/Servlet/reg.java
@@ -0,0 +1,20 @@
+package Servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+* @Description: 
+* @author: 惠
+* @date 2024年1月11日 下午3:10:02
+*/
+public class reg extends HttpServlet{
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+}
